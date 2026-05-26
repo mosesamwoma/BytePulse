fcd9342958e58f55b2feb12d21d2040e8cfdfd1b
Moses Amwoma <mosesamwoma@gmail.com>
2026-05-26 14:04:47 +0300

    refactor: remove run-tracker.sh script as part of project restructuring

--- linux/run-tracker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/mosesamwoma/projects/BytePulse
-source /home/mosesamwoma/projects/BytePulse/linux/venv/bin/activate
-export PYTHONPATH=/home/mosesamwoma/projects/BytePulse
-exec python3 /home/mosesamwoma/projects/BytePulse/linux/src/tracker.py
